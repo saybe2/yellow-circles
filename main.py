305213325508b80eab98b28ccff594fe66dd68b6
saybe <vanuya.krak@gmail.com>
2025-12-10 22:44:33 +0300

    remove UI.ui, add Ui class and random colors

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
 import sys
 import random
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtCore import QPointF
-from PyQt6 import uic
 
 
-class MainWindow(QMainWindow):
+class Ui_MainWindow:
+    def setupUi(self, MainWindow):
+        MainWindow.resize(600, 400)
+        MainWindow.setWindowTitle("Random Circles")
+        self.centralwidget = QWidget(MainWindow)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.pushButton = QPushButton('Add Circle', self.centralwidget)
+        self.pushButton.setGeometry(250, 20, 100, 30)
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.circles = []
         self.pushButton.clicked.connect(self.add_circle)
 
     def add_circle(self):
         x = random.randint(50, self.width() - 50)
         y = random.randint(50, self.height() - 50)
-        diameter = random.randint(20, 100)
-        self.circles.append((x, y, diameter))
+        d = random.randint(20, 100)
+        color = QColor(random.randint(0, 255), random.randint(
+            0, 255), random.randint(0, 255))
+        self.circles.append((x, y, d, color))
         self.update()
 
     def paintEvent(self, event):
         painter = QPainter(self)
         painter.setRenderHint(QPainter.RenderHint.Antialiasing)
-        for x, y, d in self.circles:
-            painter.setBrush(QColor(255, 255, 0))
-            painter.drawEllipse(QPointF(x, y), d/2, d/2)
+        for x, y, d, color in self.circles:
+            painter.setBrush(color)
+            painter.drawEllipse(QPointF(x, y), d / 2, d / 2)
 
 
 if __name__ == '__main__':
